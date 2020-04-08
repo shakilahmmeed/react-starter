baecb4de9b53894f474702d6e5c81e0ef4ec78cf
shakilahmmeed <shakilahmmeed@gmail.com>
2020-04-08 13:59:53 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CakeContainer from './components/CakeContainer/CakeContainer'
+
+function App() {
+  return (
+    <div>
+      <CakeContainer />
+      <h1>wow this is awesome</h1>
+    </div>
+  );
+}
+
+export default App;
